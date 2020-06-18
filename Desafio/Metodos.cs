e57e70e1f24a6f14cb9d87b50fe0652559aae0e5
Fuli <gabriel_fuli308@hotmail.com>
2020-06-18 04:19:52 -0300

    First Commit

---
+++ Desafio/Metodos.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace desafio
+{
+    public class Metodos
+    {
+
+        public static int Preposicao(string text)
+        {
+            var list = text.Split(' ');
+            return Check(list).Count();
+        }
+
+        public static IEnumerable<string> Check(string[] strings)
+        {
+            foreach (var s in strings.ToList().Where(x => x.Length == 3))
+            {
+                if (!s.Contains("d"))
+                    switch (s[s.Length - 1])
+                    {
+                        case 's':
+                        case 'l':
+                        case 'f':
+                        case 'w':
+                        case 'k':
+                            break;
+                        default:
+                            //Console.WriteLine(s);
+                            yield return s;
+                            break;
+                    }
+            }
+        }
+
+        public static string Verbos(string x)
+        {
+            string[] strings = x.Split(' ');
+            ArrayList Verbos = new ArrayList();
+            ArrayList VerbosPrimeiraPessoa = new ArrayList();
+
+            foreach (var s in strings.ToList().Where(x => x.Length >= 8))
+            {
+                switch (s[s.Length - 1])
+                {
+                    case 's':
+                    case 'l':
+                    case 'f':
+                    case 'w':
+                    case 'k':
+                        Verbos.Add(s);
+                        switch (s[0])
+                        {
+                            case 's':
+                            case 'l':
+                            case 'f':
+                            case 'w':
+                            case 'k':
+                                break;
+                            default:
+                                VerbosPrimeiraPessoa.Add(s);
+                                break;
+                        }
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            var y = Verbos.Count.ToString();
+
+            var z = VerbosPrimeiraPessoa.Count.ToString();
+
+            string result = $"Os Verbos são : {y} e Os Verbos em Primeira Pessoa são : {z} ";
+
+            return result;
+        }
+
+        public static IEnumerable<string> Ordenar(string text)
+        {
+            var list = text.Split(' ');
+
+            var chars = new List<char>(new[]
+            {
+                'k', 'b', 'w', 'r', 'q', 'd', 'n', 'f', 'x', 'j', 'm', 'l', 'v', 'h', 't', 'c', 'g', 'z', 'p', 's'
+            });
+
+            return Sort(list.ToList(), chars);
+        }
+
+        public static IEnumerable<string> Sort(IEnumerable<string> list, IEnumerable<char> chars)
+        {
+            return chars.Join(
+                list,
+                key => key,
+                x => x[0],
+                (key, item) => item);
+        }
+
+    }
+}
---
+++ Desafio/Program.cs
@@ -0,0 +1,38 @@
+﻿/*
+ * Autor: Gabriel Fuli Valcarce
+ * Email: gabriel_fuli308@hotmail.com
+ */
+
+using System;
+using System.IO;
+
+namespace desafio
+{
+    public class Program : Metodos
+    {
+        public static void Main(string[] args)
+        {
+            /* Por Favor, Salve seu arquivo .txt com o texto em klingon
+             * e coloque o caminho em que ele se encontra em sua máquina 
+             */
+            string Ph = File.ReadAllText("C:\\Git\\Desafio\\klingon-textoB-ordenado.txt"); 
+
+            Console.WriteLine($"O numero de Preposições é : {Preposicao(Ph)} \n");
+
+            Console.WriteLine(Verbos(Ph) + "\n");
+
+            foreach (var item in Ordenar(Ph))
+            {
+                Console.Write(item + " ");
+            }
+
+            /* esse WriteLine esta aqui so para melhorar a vizualização no console
+             * então pode desconsiderar
+             */
+            Console.WriteLine("\n");
+
+            Console.WriteLine("Infelizmente não compreendi os números na base vigesimal em klingon, então essa vou ficar devendo rs");
+
+        }
+    }
+}
